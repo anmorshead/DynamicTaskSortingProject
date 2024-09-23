82c7a6dddf39b593c823885379a8c9b5ae5a7c93
Andrea Morshead <andrea.morshead@gmail.com>
2024-09-23 09:59:17 -0300

    base page up and running

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'; // This line is crucial
+
+function App() {
+    return (
+      <div>
+        <h1>This is My Dynamic Task Sorting App</h1>
+        <main>
+         <p>Testing</p>
+        </main>
+      </div>
+    );
+  }
+  
+  export default App;
